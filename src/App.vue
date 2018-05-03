42ca6c19559cb516e972a7c782ffb88204462244
liulihua <hc_liulihua@163.com>
2018-05-03 22:00:30 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <v-header></v-header>  
+    <div class="tab">
+      I am tab!
+    </div>
+    <div class="content">
+      I am content!
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+
+  export default {
+    components: {
+      'v-header':header
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
