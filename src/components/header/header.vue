821ee6c13fdebcfeba3f11e4dccaf6fbbbaab31f
liulihua <hc_liulihua@163.com>
2018-05-11 00:17:49 +0800

    seller

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,13 +13,22 @@
 					{{seller.description}}/预计{{seller.deliveryTime}}分钟送达
 				</div>
 				<div class="support" v-if="seller.supports">
-					<span class="icon"></span>
+					<span class="icon" :class="classMap[seller.supports[0].type]"></span>
 					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
+			<div v-if="seller.supports" class="support-count">
+				<span class="count">{{seller.supports.length}}个</span>
+				<i class=""></i>
+			</div>
 		</div>
 		<div class="bulletin-wrapper">
-
+			<span class="bulletin-title"></span>
+			<span class="bulletin-text">{{seller.bulletin}}</span>
+			<i class=""></i>
+		</div>
+		<div class="background">
+			<img :src="seller.avatar" alt="">
 		</div>
 	</div>
 </template>
@@ -30,66 +39,12 @@
 			seller:{
 				type: Object
 			}
+		},
+		created: function(){
+			this.classMap = ['decrease','discount','special','invoice','guarantee'];
 		}
 	}
 </script>
-
-<style  lang="scss">
-@import "../../common/scss/mixin";
-
-.header{
-	color: #fff;
-	background-color: #000;
-	.content-wrapper{
-		padding: 24px 12px 18px 24px;
-		font-size: 0;
-		.avatar{
-			display: inline-block;
-			border-radius: 2px;
-			width: 64px;
-			height: 64px;
-		}
-		.content{
-			display: inline-block;
-			margin-left: 16px;
-			font-size: 14px;
-			.title{
-				margin: 2px 0 8px 0;
-				font-size: 16px;
-				font-weight: bold;
-				display: flex;
-				align-items: center;
-				/*line-height: 36px;*/
-				.brand{
-					display: inline-block;
-					width: 30px;
-					height: 18px;
-					@include bg-image('../../../static/image/header/brand');
-					background-size: 30px 18px;
-					background-repeat: no-repeat;
-				}
-				.name{
-					margin-left:6px;
-					line-height:18px;
-				}
-			}
-			.description{
-				margin-bottom: 10px;
-				line-height: 12px;
-			}
-			.support{
-				font-size: 10px;
-				font-weight: 200;
-				line-height: 24px;
-			}
-		}
-	}
-}
-
-
-
-
-
-
-
+<style lang="scss">
+	@import "../../common/scss/header";
 </style>
